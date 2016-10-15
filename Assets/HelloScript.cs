87444509370db1201fc404913c9a8860238777e8
INHEEKIM <tie_ara@naver.com>
2016-10-15 12:28:35 +0900

    Add files via upload

---
+++ Assets/HelloScript.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+
+public class HelloScript : MonoBehaviour
+{
+
+    private Text hellotext;
+
+    // Use this for initialization
+    void Start()
+    {
+        hellotext = GameObject.Find("Text").GetComponent<Text>();
+        hellotext.text = "Hey Yo! Man";
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+    }
+}
